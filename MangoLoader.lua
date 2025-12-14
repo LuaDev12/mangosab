8c102782c44682a42c1a7b7ed4ecf163267ff0f1
LuaDev12 <chamnannoreakvisnouk@gmail.com>
2025-12-14 16:51:57 +0700

    Update MangoLoader.lua

--- MangoLoader.lua
+++ MangoLoader.lua
@@ -1,4 +1,7 @@
+-- Print The Messages
 print("🥭 Mango Hub Steal A Brainrot 🥭")
 print("🥭 Mango Hub Loader 1.0 🥭")
+-- Loads The Script
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c4281c3937ebd537cb9e860182e41141.lua"))()
+-- Notify That Is Loaded
 print("🥭 Mango Hub Loaded! 🥭")
